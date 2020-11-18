0aeef35b60f19f233e3dfacae8a2311e58325ba7
Julien Bigot <julien.bigot@cea.fr>
2020-11-18 18:44:59 +0100

    update dssat-csm-os

--- dssat-run.sh
+++ dssat-run.sh
@@ -7,6 +7,9 @@ echo -e
 echo -e "\e[1mnext, \e[32mto start the execution\e[39m, in another terminal, run:\e[0m"
 echo -e "$ docker exec -ti ${HOSTNAME} flowvr csm"
 echo -e
+echo -e "\e[1if you prefer, \e[32mto enter the container\e[39m, in another terminal, run:\e[0m"
+echo -e "$ docker exec -ti ${HOSTNAME} bash -li"
+echo -e
 echo -e "\e[1monce done, \e[31mto stop the daemon\e[39m, in another terminal, run:\e[0m"
 echo -e "$ docker stop ${HOSTNAME}"
 echo -e
